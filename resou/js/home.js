c0e309b18612c2b7e55392248b431863ea1488c5
[v3u3i87] <[code@zmq.cc]>
2016-03-09 16:18:12 +0800

    设置左右切换

--- resou/js/home.js
+++ resou/js/home.js
@@ -8,24 +8,48 @@ define(['ku','alert','api'],function (ku,msg,api) {
         if(projectList && projectList.code == '200')
         {
             viewProjectList(projectList.data);
-
             $(".item").click(function(){
                 var project_id = $(this).data('project_id');
                 var designList = api.designList(project_id);
-                if(designList && designList.code=='200') {
-                    $(".main").empty();
-                    var project = designList.data.project;
-                    $(".title_info").empty().text('返回');
-                    $(".title_name").empty().text(project.name);
-                    viewDesignList(designList.data);
-                }else{
-                    msg.info('该项目没有设计工作区');
-                }
-
+                designListLogic(designList,projectList.data);
             });
+
         }
     }
 
+    /**
+     * 设计列表逻辑
+     * @param designList
+     */
+    function designListLogic(designList,projectList)
+    {
+        if(designList && designList.code=='200') {
+            $(".main").empty();
+            var project = designList.data.project;
+            $(".title_info").empty().text('返回').css({"cursor":"pointer"});
+            $(".title_name").empty().text(project.name);
+            viewDesignList(designList.data);
+            clickTitleInfo(projectList);
+        }else{
+            msg.info('该项目没有设计工作区');
+        }
+    }
+
+
+    /**
+     * 点击标题信息事件
+     * 删除当前项目设计工作区
+     */
+    function clickTitleInfo(projectList)
+    {
+        $(".title_info").click(function(){
+            console.log('info_click');
+            $(".title_info").empty().text('项目公共');
+            $(".title_name").empty().text('操作区域');
+            $(".main").empty();
+            viewProjectList(projectList);
+        });
+    }
 
     /**
      * 渲染设计列表
@@ -52,15 +76,14 @@ define(['ku','alert','api'],function (ku,msg,api) {
     function viewProjectList(v)
     {
         console.log(v);
-        var h = '';
+        var h = '<ul class="projectList">';
         for(var i=0;i < v.length;i++)
         {
             h+='<li class="item" data-project_id="'+v[i].project_id+'">'+v[i].name+'</li>';
         }
-        $(".projectList").empty().html(h);
+        h+='</ul>';
+        $(".main").empty().html(h);
     }
-
-
 
     return {
         init:init
